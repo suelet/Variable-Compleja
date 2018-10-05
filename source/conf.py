aacd9cd52845c577ec156f91a74dae211325ee0d
Jose Luis Hernandez Schweicez <jose.lui.hernandez-schweicez@hpe.com>
2018-10-05 15:41:46 +0200

    Versión Inicial

--- source/conf.py
+++ source/conf.py
@@ -130,7 +130,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'Variable-Compleja.tex', 'Variable-Compleja Documentation',
-     'José Luis Hernández Schweicez', 'manual'),
+     u'José Luis Hernández Schweicez', 'manual'),
 ]
 
 
